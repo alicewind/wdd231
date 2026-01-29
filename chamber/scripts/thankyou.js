e6df5bbf8430f470d044bb385a74af464a6e8256
alicewind <awind@byupathway.edu>
2026-01-29 11:38:08 -0700

    thank you styling

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -1,11 +1,11 @@
 const formInfo = new URLSearchParams(window.location.search);
 
 document.querySelector("#info").innerHTML = `
-<p>Name: ${formInfo.get("first")} ${formInfo.get("last")}</p>
-<p>Email: ${formInfo.get("email")}</p>
-<p>Phone: ${formInfo.get("phone")}</p>
-<p>Business/Organization: ${formInfo.get("organization-name")}</p>
-<p>Membership Level: ${formInfo.get("radio")}</p>
-<p>Submitted On: ${formInfo.get("timestamp")}</p>`;
+<p><strong>Name: </strong>${formInfo.get("first")} ${formInfo.get("last")}</p>
+<p><strong>Email: </strong>${formInfo.get("email")}</p>
+<p><strong>Phone: </strong>${formInfo.get("phone")}</p>
+<p><strong>Business/Organization: </strong>${formInfo.get("organization-name")}</p>
+<p><strong>Membership Level: </strong>${formInfo.get("radio")}</p>
+<p><strong>Submitted On: </strong>${formInfo.get("timestamp")}</p>`;
 
 
